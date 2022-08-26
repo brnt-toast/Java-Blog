46fb9a277cc7a7c3490c7239772f30263ef84359
brnt-toast <yoyo.beerme@gmail.com>
2022-08-26 11:54:16 -0400

    Models/init: Post.java and User.java

---
+++ blog/src/main/java/com/blog/blog/model/Post.java
@@ -0,0 +1,41 @@
+package com.blog.blog.model;
+
+public class Post {
+    private String title;
+    private String content;
+    private String author;
+    public long id;
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+}
